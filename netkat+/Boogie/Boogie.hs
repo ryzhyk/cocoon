f6e335a2189faf03b6b4573c973080eef1cbd809
Leonid Ryzhyk <l.ryzhyk@samsung.com>
2016-03-31 09:54:39 -0700

    bug fixes

--- netkat+/Boogie/Boogie.hs
+++ netkat+/Boogie/Boogie.hs
@@ -65,20 +65,25 @@ refinementToBoogie1 abst conc rname = vcat $ punctuate (pp "") [ops, types, gvar
           crmap  = M.fromList $ map (\n -> (n, "c_" ++ n)) new
           types  = vcat $ (map mkTypeDef $ refineTypes conc) ++ [mkLocType conc, mkOutputType]
           gvars  = vcat $ [ var (pp "abs") (pp outputTypeName)
-                          , var (pp "conc") (pp outputTypeName)
-                          , var (pp "inppkt") (pp packetTypeName)]
+                          , var (pp "conc") (pp outputTypeName)]
           funcs  = vcat $ map (mkFunction conc) $ refineFuncs conc
           aroles = let ?rmap = armap 
                        ?ovar = "abs" in 
-                   vcat $ punctuate (pp "") $ map (mkRole abst) $ refineRoles abst
+                   vcat $ punctuate (pp "") $ map (mkRole abst) $ map (getRole abst) tgts
           croles = let ?rmap = crmap 
                        ?ovar = "conc" in 
-                   vcat $ punctuate (pp "") $ map (mkRole conc) $ refineRoles conc
+                   vcat $ punctuate (pp "") $ map (mkRole conc) $ map (getRole conc) new
           assums = vcat $ map (mkAssume conc) $ refineAssumes conc
           check  = mkCheck conc rname 
 
 mkCheck :: Refine -> String -> Doc
-mkCheck conc rname = (pp "procedure" <+> pp "main" <+> parens empty <+> lbrace)
+mkCheck conc rname = (pp "procedure" <+> pp "main" <+> parens empty)
+                     $$
+                     (modifies $ pp "abs")
+                     $$
+                     (modifies $ pp "conc")
+                     $$
+                     lbrace
                      $$
                      (nest' body)
                      $$
@@ -86,28 +91,28 @@ mkCheck conc rname = (pp "procedure" <+> pp "main" <+> parens empty <+> lbrace)
     where role = getRole conc rname
           body = (vcat $ map ((pp "var" <+>) . (<> semi) . mkField) $ roleKeys role)
                  $$
+                 (var (pp "inppkt") (pp packetTypeName))
+                 $$
                  (vcat $ map (\f -> pp "havoc" <+> (pp $ name f) <> semi) $ roleKeys role)
                  $$
                  (pp "assume" <+> (mkExpr conc (CtxRole role) $ roleKeyRange role) <> semi)
                  $$
                  (pp "havoc" <+> pp "inppkt" <> semi)
                  $$
-                 (pp "abs" =: pp "Dropped")
+                 (pp "abs" =: (pp "Dropped" <> (parens $ empty)))
                  $$
-                 (pp "conc" =: pp "Dropped")
+                 (pp "conc" =: (pp "Dropped" <> (parens $ empty)))
                  $$
-                 (vcat $ map mkField $ roleKeys role)
+                 (pp "call" <+> (pp $ "a_" ++ rname) <> (parens $ hsep $ punctuate comma $ (map (pp . name) $ roleKeys role) ++ [pp "inppkt"]) <> semi)
                  $$
-                 (pp $ "a_" ++ rname)
-                 $$
+                 (pp "call" <+> (pp $ "c_" ++ rname) <> (parens $ hsep $ punctuate comma $ (map (pp . name) $ roleKeys role) ++ [pp "inppkt"]) <> semi)
+                 $$ 
                  (pp "assert" <+> pp "abs" <+> pp "==" <+> pp "conc" <> semi)
 
 mkLocType :: Refine -> Doc
 mkLocType r = (pp "type" <+> pp "{:datatype}" <+> pp locTypeName <> semi)
               $$
               (vcat $ map mkLocConstructor $ refineRoles r)
-              $$
-              (pp "function" <+> pp "{:constructor}" <+> pp "Dropped" <> (parens $ empty) <> colon <+> pp locTypeName <>  semi)
 
 mkLocConstructor :: Role -> Doc
 mkLocConstructor rl = pp "function" <+> pp "{:constructor}" <+> pp (name rl)
@@ -118,9 +123,11 @@ mkOutputType :: Doc
 mkOutputType = (pp "type" <+> pp "{:datatype}" <+> pp outputTypeName <> semi)
                $$
                (pp "function" <+> pp "{:constructor}" <+> pp outputTypeName
-               <> (parens $ hsep $ punctuate comma $ [ pp "pkt" <> colon <+> pp packetTypeName
-                                                     , pp "loc" <> colon <+> pp locTypeName])
-               <> colon <+> pp outputTypeName <> semi)
+                <> (parens $ hsep $ punctuate comma $ [ pp "pkt" <> colon <+> pp packetTypeName
+                                                      , pp "loc" <> colon <+> pp locTypeName])
+                <> colon <+> pp outputTypeName <> semi)
+               $$
+               (pp "function" <+> pp "{:constructor}" <+> pp "Dropped" <> (parens $ empty) <> colon <+> pp outputTypeName <> semi)
 
 mkRoleName :: (?rmap::RMap) => String -> Doc
 mkRoleName n = case M.lookup n ?rmap of
@@ -157,12 +164,18 @@ mkRole r rl@Role{..} = (pp "procedure" <+> mkRoleName roleName <+> (parens $ hse
                        $$
                        (pp "requires" <+> mkExpr r (CtxRole rl) roleKeyRange <> semi)
                        $$
-                       (pp "modifies" <+> pp ?ovar <> semi <> lbrace)
+                       (modifies $pp ?ovar)
                        $$
-                       (nest' $ mkStatement r rl roleBody)
+                       lbrace
+                       $+$
+                       (nest' $ var (pp pktVar) (pp packetTypeName) 
+                                $$
+                                pp pktVar =: pp "_pkt"
+                                $$
+                                mkStatement r rl roleBody)
                        $$
                        rbrace
-    where args = map mkField $ roleKeys ++ [Field nopos pktVar $ TUser nopos packetTypeName]
+    where args = map mkField $ roleKeys ++ [Field nopos "_pkt" $ TUser nopos packetTypeName]
 
 mkField :: Field -> Doc
 mkField f = (pp $ name f) <> colon <+> (mkType $ fieldType f)
@@ -221,13 +234,13 @@ mkStatement r rl (SITE _ c t e) = (pp "if" <> (parens $ mkExpr r (CtxRole rl) c)
                                          e)
 mkStatement r rl (STest _ c)    = pp "if" <> (parens $ mkExpr r (CtxRole rl) (EUnOp nopos Not c)) <+> (braces $ pp "return" <> semi)
 mkStatement r rl (SSet _ l rhs) = mkAssign r rl l [] rhs
-mkStatement r rl  (SSend _ dst)  = let ELocation _ rname ks = dst in
-                                   case M.lookup rname ?rmap of
-                                        Nothing -> pp ?ovar =: (pp outputTypeName <> 
-                                                                (parens $ pp pktVar <> comma <+>
-                                                                         (pp rname <> (parens $ hsep $ punctuate comma $ map (mkExpr r (CtxRole rl)) ks))))
-                                        Just _  -> mkRoleName rname <> (parens $ hsep $ punctuate comma 
-                                                                        $ map (mkExpr r (CtxRole rl)) $ ks ++ [EPacket nopos]) <> semi
+mkStatement r rl (SSend _ dst)  = let ELocation _ rname ks = dst in
+                                  case M.lookup rname ?rmap of
+                                       Nothing -> pp ?ovar =: (pp outputTypeName <> 
+                                                               (parens $ pp pktVar <> comma <+>
+                                                                        (pp rname <> (parens $ hsep $ punctuate comma $ map (mkExpr r (CtxRole rl)) ks))))
+                                       Just _  -> pp "call" <+> mkRoleName rname <> 
+                                                  (parens $ hsep $ punctuate comma $ map (mkExpr r (CtxRole rl)) $ ks ++ [EPacket nopos]) <> semi
 
 mkAssign :: Refine -> Role -> Expr -> [String] -> Expr -> Doc
 mkAssign rf rl (EField _ e f) fs r = mkAssign rf rl e (fs ++ [f]) r
@@ -292,3 +305,6 @@ exprCollectOps _ _ = []
 
 var :: Doc -> Doc -> Doc
 var n t = pp "var" <+> n <> colon <+> t <> semi
+
+modifies :: Doc -> Doc
+modifies v = pp "modifies" <+> v <> semi
