04297dfc9d7164c1be21fa1b9544e68338a7f9e7
Leonid Ryzhyk <l.ryzhyk@samsung.com>
2016-06-24 10:50:31 -0700

    improve test_verification.sh output; increase model checking bound to 15

--- test_verification.sh
+++ test_verification.sh
@@ -4,9 +4,17 @@ THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $THIS_DIR/env.sh
 
-TESTS=(convergence b4 f10)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+TESTS=(stags convergence b4 f10)
 
 for t in ${TESTS[@]}; do
-        ${COCOON_PATH} examples/${t}.ccn 10
-        $THISDIR/tools/run_corral.sh examples/${t}/boogie
+        printf "${GREEN}Starting test: ${t}${NC}\n"
+        ${COCOON_PATH} examples/${t}.ccn 15
+        if [ $? -eq 0 ]
+        then
+            $THISDIR/tools/run_corral.sh examples/${t}/boogie
+        fi
 done
