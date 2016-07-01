3bacbc9b5def36cfb95162fcfd11b8a1cad80f7f
Leonid Ryzhyk <l.ryzhyk@samsung.com>
2016-07-01 10:56:26 -0700

    invoke m4 for tests that require preprocessor in test_verification.sh

--- test_verification.sh
+++ test_verification.sh
@@ -8,13 +8,17 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-TESTS=(stags convergence b4 f10)
+TESTS=(isdx.m4 stags convergence b4 f10)
 
 for t in ${TESTS[@]}; do
         printf "${GREEN}Starting test: ${t}${NC}\n"
-        ${COCOON_PATH} examples/${t}.ccn 15
+        if [[ ${t} == *.m4 ]]
+        then
+            m4 examples/${t}.ccn > examples/${t%%.*}.ccn
+        fi
+        ${COCOON_PATH} examples/${t%%.*}.ccn 15
         if [ $? -eq 0 ]
         then
-            $THISDIR/tools/run_corral.sh examples/${t}/boogie
+            $THISDIR/tools/run_corral.sh examples/${t%%.*}/boogie
         fi
 done
