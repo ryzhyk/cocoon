6bc68e4399eb3c44dec6082ad54f74169f59e9c5
Leonid Ryzhyk <l.ryzhyk@samsung.com>
2016-07-11 14:14:07 -0700

    [virtualization] split into generic and VxLAN-specific parts

--- test_verification.sh
+++ test_verification.sh
@@ -8,17 +8,18 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-TESTS=(isdx.m4 stags convergence b4 f10)
+TESTS=(vxlan_virt.m4 isdx.m4 stags b4 f10)
 
 for t in ${TESTS[@]}; do
         printf "${GREEN}Starting test: ${t}${NC}\n"
         if [[ ${t} == *.m4 ]]
         then
-            m4 examples/${t}.ccn > examples/${t%%.*}.ccn
+            m4 -I examples examples/${t}.ccn > examples/${t%%.*}.ccn
         fi
         ${COCOON_PATH} examples/${t%%.*}.ccn 15
         if [ $? -eq 0 ]
         then
             $THISDIR/tools/run_corral.sh examples/${t%%.*}/boogie
         fi
+        printf "\n"
 done
