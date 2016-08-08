d0b25cf89f0b093969d5ecddadf0e543580e1887
Leonid Ryzhyk <l.ryzhyk@samsung.com>
2016-08-08 09:39:12 -0700

    simple fat tree example for the paper
    This will probably end up not being used in the paper

--- test_verification.sh
+++ test_verification.sh
@@ -8,7 +8,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-TESTS=(vlan_virt.m4 vxlan_virt.m4 isdx.m4 stags b4 f10)
+TESTS=(simple-fat-tree vlan_virt.m4 vxlan_virt.m4 isdx.m4 stags b4 f10)
 
 for t in ${TESTS[@]}; do
         printf "${GREEN}Starting test: ${t}${NC}\n"
