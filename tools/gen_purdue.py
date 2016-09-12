10b265ec2fd6235186c45fb5dfd771c63f4a2348
Cole Schlesinger <cole.s@samsung.com>
2016-09-12 11:43:56 -0700

    Add cRouterPort.

--- tools/gen_purdue.py
+++ tools/gen_purdue.py
@@ -1374,6 +1374,17 @@ function cSwitchPort(pid_t pid): bool =
     {pids}
 '''.format(pids=pids))
 
+        # FUNCTION cPort
+        pids = ["pid == pid_t{64'd%d, 16'd%d}" % (n, port)
+                for n in g
+                for port in g.node[n]['ports'].values()
+                if g.node[n]['type'] == 'router']
+        pids = ' or\n    '.join(pids)
+        out.write('''
+function cRouterPort(pid_t pid): bool = 
+    {pids}
+'''.format(pids=pids))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
