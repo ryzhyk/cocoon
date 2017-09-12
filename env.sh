b9d9155b8bb57c40d688cde9312e9765c3d87810
Leonid Ryzhyk <lryzhyk@vmware.com>
2017-09-12 13:20:57 -0700

    bootstrap.sh script wip

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # ---------------- EDIT THIS ------------------
-BMV2_PATH=$THISDIR/p4/bmv2
-P4C_BM_PATH=$THISDIR/p4/p4c-bmv2
+#BMV2_PATH=$THISDIR/p4/bmv2
+#P4C_BM_PATH=$THISDIR/p4/p4c-bmv2
 COCOON_PATH=$THISDIR/cocoon/dist/build/cocoon/cocoon
-MINIEDIT_PATH=$THISDIR/tools/miniedit.py
+#MINIEDIT_PATH=$THISDIR/tools/miniedit.py
 # ---------------- END ------------------
 
---
+++ tools/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# bootstrap a Cocoon network
+
+set -x
+set -e
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source $THIS_DIR/../env.sh
+
+filename=$(basename "$1")
+specname="${filename%.*}"
+dir=$(dirname "$1")
+workdir=$dir/$specname
+
+echo "Compiling $specname"
+
+# kill cocoon controller and CLI (if any)
+set +e
+killall -qw -9 cocoon
+set -e
+
+# create SQL and Datalog scripts
+(set +x; echo Generating  SQL schema)
+$COCOON_PATH -i $1 --action=sql
+
+(set +x; echo Generating Datalog rules)
+$COCOON_PATH -i $1 --action=dl
+
+(set +x; echo Initializing SQL DB)
+psql -f $workdir/$specname.schema
+
+(set +x; echo Compiling Datalog rules)
+pushd $workdir
+cargo build
+pushd $popd
