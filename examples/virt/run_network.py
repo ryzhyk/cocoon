335d45eeaf1c1fda16357067dd6d5d5acdeff756
Leonid Ryzhyk <lryzhyk@vmware.com>
2017-09-21 16:10:03 -0700

    [virt] Packets get delivered via vxlan tunnel across physical nodes

--- examples/virt/run_network.py
+++ examples/virt/run_network.py
@@ -28,6 +28,7 @@ import signal
 import subprocess
 import time
 import sys
+import netaddr
 
 #_THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -41,17 +42,17 @@ parser.add_argument('--topology', help='MiniEdit topology file',
 
 args = parser.parse_args()
 
-class LinuxRouter( Node ):
-    "A Node with IP forwarding enabled."
-
-    def config( self, **params ):
-        super( LinuxRouter, self).config( **params )
-        # Enable forwarding on the router
-        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
-
-    def terminate( self ):
-        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
-        super( LinuxRouter, self ).terminate()
+#class LinuxRouter( Node ):
+#    "A Node with IP forwarding enabled."
+#
+#    def config( self, **params ):
+#        super( LinuxRouter, self).config( **params )
+#        # Enable forwarding on the router
+#        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+#
+#    def terminate( self ):
+#        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+#        super( LinuxRouter, self ).terminate()
 
 
 def cocoon(cmd):
@@ -72,8 +73,13 @@ class VNet (Mininet):
         hostname = "h" + str(hostid)
         s = self.addSwitch(hostname)
         s.start([])
+        vxportname = hostname + "-vx"
         s.cmd(["ovs-vsctl", "set", "bridge", hostname, "protocols=OpenFlow15"])
+        s.cmd(["ovs-vsctl", "add-port", hostname, vxportname, "--", "set", "interface", vxportname, "type=vxlan", "options:remote_ip=flow", "options:local_ip="+hostip, "options:key=flow"])
+        portnum = s.cmd(["ovs-vsctl", "get", "Interface", vxportname, "ofport"])
+        #self.addLink(s, self.get("r0"), intfName1="vx", intfName2="r0-"+hostname, params2={'ip': hostip+"/32"})
         cocoon("Hypervisor.put(Hypervisor{" + str(hostid) + ", false, \"" + hostname + "\", \"\"})")
+        cocoon("HypervisorTunPort.put(HypervisorTunPort{" + str(hostid) + ", " + str(int(portnum)) + ", " + str(hostid) + ", " + str(int(netaddr.IPAddress(hostip))) + "})")
         return
 
     def delHost(self, hostid):
@@ -103,7 +109,7 @@ class VNet (Mininet):
         h.cmd("sysctl -w net.ipv4.tcp_congestion_control=reno")
         h.cmd("ifconfig eth0 promisc")
         h.cmd("iptables -I OUTPUT -p icmp --icmp-type destination-unreachable -j DROP")
-        cocoon("HypervisorPort.put(HypervisorPort{" + str(vmid) + "," + str(portnum) + "," + str(host) + "})")
+        cocoon("HypervisorPort.put(HypervisorPort{" + str(vmid) + "," + str(int(portnum)) + "," + str(host) + "})")
         return
 
     def delVM(self, vmid):
@@ -112,20 +118,26 @@ class VNet (Mininet):
 def main():
 
     cocoon(":connect")
-    # Create empty topology with Linux router
+
+    subprocess.check_call(["rmmod", "dummy"])
+    subprocess.check_call(["sudo", "modprobe", "dummy", "numdummies=10"])
+
     topo = Topo()
-    router = topo.addNode('r0', cls=LinuxRouter)
     net = VNet(topo=topo, controller = None)
     net.start()
 
-    cocoon("LogicalSwitch.put(LogicalSwitch{0})")
+    cocoon("LogicalSwitch.put(LogicalSwitch{123})")
+
+    subprocess.check_call(["ifconfig", "dummy0", "10.10.10.101"])
     net.addHV(1, "10.10.10.101")
+    subprocess.check_call(["ifconfig", "dummy1", "10.10.10.102"])
+    net.addHV(2, "10.10.10.102")
 
-    net.addVM(1, 1, '00:00:c6:e2:a3:01', "192.168.0.1")
-    cocoon("LogicalPort.put(LogicalPort{0,0,1,48'h112233445566})")
+    net.addVM(1, 1, '11:22:33:44:55:66', "192.168.0.1")
+    cocoon("LogicalPort.put(LogicalPort{0,123,1,48'h112233445566})")
 
-    net.addVM(2, 1, '00:00:c6:e2:a3:02', "192.168.0.2")
-    cocoon("LogicalPort.put(LogicalPort{1,0,2,48'h112233445577})")
+    net.addVM(2, 2, '11:22:33:44:55:77', "192.168.0.2")
+    cocoon("LogicalPort.put(LogicalPort{1,123,2,48'h112233445577})")
 
     CLI(net)
 
